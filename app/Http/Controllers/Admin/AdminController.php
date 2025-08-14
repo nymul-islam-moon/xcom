bff6e477e01f2b2df4d1e1bf7efe44fff6922abb
Nymul Islam <nymulislam.dev@gmail.com>
2025-08-14 14:34:17 +0600

    controller adding

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\StoreAdminRequest;
-use App\Http\Requests\UpdateAdminRequest;
+use App\Http\Requests\Admin\Auth\StoreAdminRequest;
+use App\Http\Requests\Admin\Auth\UpdateAdminRequest;
 use App\Models\Admin;
 
 class AdminController extends Controller
